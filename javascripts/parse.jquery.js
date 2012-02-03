d6e7edd6e77e5e75962f6ddcc37018e2173fec5b
AmatsukiKu <amatsukikuu.gmail.com>
2012-02-03 18:17:57 +0900

    display applying range on lyrics

--- javascripts/parse.jquery.js
+++ javascripts/parse.jquery.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
     var changeHighlight = function(obj){
-        var index = $("input:checkbox").index(obj);
+        var index = $(obj).parent().find("input:checkbox").index(obj);
         if(index !== -1){
-            var range = $("#range" + index);
+            console.log($(obj).val())
+            var range = $(obj).parent().find("#range" + $(obj).val());
             if($(obj).attr("checked") === "checked"){
                 range.addClass("choosen");
             } else {
@@ -10,6 +11,15 @@ $(document).ready(function(){
             }
         }
     }
+    var selectRule = function(obj){
+        if($(obj).attr("checked") === "checked"){
+            $(".rule").css("display", "none");
+            $("#rule" + $(obj).val()).css("display", "block");
+        }
+    }
     $("input:checkbox").change(function(){ changeHighlight(this) });
     $("input:checkbox").each(function(){ changeHighlight(this)} );
+    $("input:radio").change(function(){ selectRule(this) });
+    $(".rule").css("display", "none");
+    $("input:radio").each(function(){ selectRule(this)} );
 });
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from google.appengine.ext.webapp import template
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.api import memcache
-from vsq import VSQEditor
+from vsq import *
 
 class MainPage(webapp.RequestHandler):
     def get(self):
@@ -22,13 +22,28 @@ class ParserPage(webapp.RequestHandler):
         data = self.request.get('file')
         file_name = self.request.body_file.vars['file'].filename
         editor = VSQEditor(string = data)
-        anotes = editor.get_anotes()
-        lyrics = [anote['lyrics'] for anote in anotes]
+        #anotes = editor.get_anotes()
+        #lyrics = [anote['lyrics'] for anote in anotes]
+        lyrics = editor.get_lyrics()
+        candidates = editor.get_rule_cands(san_rule)
+
+        output_lyric = ""
+        before_index = 0
+        for key, value in sorted(candidates.items(), key=lambda x:x[1]["s_index"]):
+            s_index = value["s_index"]
+            e_index = value["e_index"]
+            output_lyric += lyrics[before_index:s_index].encode('utf-8') if (s_index != 0) else ""
+            output_lyric += "<span id=\"range"+key+"\" class=\"chooseable\">"+ lyrics[s_index:e_index].encode('utf-8') + "</span>"
+            brefore_index = e_index
+        output_lyric += lyrics[before_index:].encode('utf-8') if (before_index != len(lyrics)) else ""
+
         memcache.set_multi({ "data": data,
             "name": file_name },
             key_prefix="vsq_", time=3600)
         template_values = {
-                'lyrics': ''.join(lyrics).decode('shift_jis').encode('utf-8')
+                #'lyrics': ''.join(lyrics).decode('shift_jis').encode('utf-8')
+                'lyrics': output_lyric,
+                'keys': candidates.keys()
                 }
         path = os.path.join(os.path.dirname(__file__), 'parse.html')
         self.response.out.write(template.render(path, template_values))
@@ -37,14 +52,19 @@ class ParserPage(webapp.RequestHandler):
 
 class DownloadPage(webapp.RequestHandler):
     def post(self):
-        applied_range = self.request.get_all('range')
-        str = "".join(applied_range)
         data = memcache.get("vsq_data")
         file_name = memcache.get("vsq_name")
-        self.response.headers['Content-Type'] = "application/x-vsq; charset=Shift_JIS"
-        self.response.headers['Content-disposition'] = "filename=" + file_name.encode("utf-8")
         editor = VSQEditor(string = data)
-        self.response.out.write(editor.unparse())
+        keys = self.request.get_all("rule1")
+        print("".join(keys))
+
+        #for i in range(1, len(rules))
+            #targets = self.request.get_all("rule" + i)
+            #for j in targets
+                #editor.apply_rule(rules[i], candidates[int(j)])
+        #self.response.headers['Content-Type'] = "application/x-vsq; charset=Shift_JIS"
+        #self.response.headers['Content-disposition'] = "filename=" + file_name.encode("utf-8")
+        #self.response.out.write(editor.unparse())
 
 application = webapp.WSGIApplication(
                                         [('/', MainPage),
